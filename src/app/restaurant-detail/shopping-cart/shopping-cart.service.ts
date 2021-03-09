0da527e27a12d73569ef00b664af1ed7ab90222f
rangellarissa <larissarangel.oc@gmail.com>
2021-03-09 18:55:46 -0300

    configurando adição e remoção de itens na página de pedido, assim como total da compra

--- src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -16,12 +16,24 @@ export class ShoppingCartService{
     addItem(item:MenuItem){
         let foundItem = this.items.find((mItem)=> mItem.menuItem.id === item.id);
         if(foundItem){
-            foundItem.quantity = foundItem.quantity + 1
+            this.increaseQty(foundItem)
         }else{
             this.items.push(new CartItem(item))
         }
 
     }
+
+    increaseQty(item: CartItem){
+        item.quantity = item.quantity + 1
+    }
+
+    decreaseQty(item: CartItem){
+        item.quantity = item.quantity - 1
+        if (item.quantity === 0){
+            this.removeItem(item)
+        }
+    }
+
 //o método removeItem utiliza o método splice para remover o item do carrinho
     removeItem(item:CartItem){
         this.items.splice(this.items.indexOf(item), 1)
